8f9a92bcc6e6b9c8c4643488ec03e98be5021c4b
root <root@.localdomain>
2021-01-13 20:43:18 +0800

    改了改

--- check.py
+++ check.py
@@ -1,12 +1,16 @@
-import json,os
+import json
+import os
+
 answer_str = "answer"
 fileList = os.listdir(answer_str)
-
+ok = True
 for filename in fileList:
     with open(os.path.join(answer_str,filename)) as answerFile:
         with open(filename) as resultFile:
             answer = json.loads(answerFile.read())
             result = json.loads(resultFile.read())
-            print(filename)
-            assert answer==result
-            print("OK")
+            if answer!=result:
+                print(filename+" is not same as provided.")
+                ok=False
+
+if ok: print("ok, all the same")
--- run.sh
+++ run.sh
@@ -1,13 +1,16 @@
-python3 main.py --node a &\
-python3 main.py --node b &\
-python3 main.py --node c &\
-python3 main.py --node d &\
-python3 main.py --node e &\
-python3 main.py --node f &\
-python3 main.py --node g &\
-python3 main.py --node h &\
-python3 main.py --node i &\
-python3 main.py --node j &\
-python3 main.py --node k &\
-python3 main.py --node l &\
-
+python3 main.py --node a &
+python3 main.py --node b &
+python3 main.py --node c &
+python3 main.py --node d &
+python3 main.py --node e &
+python3 main.py --node f &
+python3 main.py --node g &
+python3 main.py --node h &
+python3 main.py --node i &
+python3 main.py --node j &
+python3 main.py --node k &
+python3 main.py --node l &
+echo "在跑了"
+wait
+echo "跑好了"
+python3 check.py
