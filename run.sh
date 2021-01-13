ceac520d41d662923aaead4402210a664cff23ee
lynkas <cat@catting.net>
2021-01-13 20:25:30 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,13 @@
+python3 main.py --node a &\
+python3 main.py --node b &\
+python3 main.py --node c &\
+python3 main.py --node d &\
+python3 main.py --node e &\
+python3 main.py --node f &\
+python3 main.py --node g &\
+python3 main.py --node h &\
+python3 main.py --node i &\
+python3 main.py --node j &\
+python3 main.py --node k &\
+python3 main.py --node l &\
+
